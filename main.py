40cc9d0a2bc93d72c30d4b59f6224817fda4c977
ganesh500 <73403074+ganesh500@users.noreply.github.com>
2020-10-25 11:52:31 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from Flask import Flask, render_template
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route ("/")
 def index():
-  headline = "Tracker"
+  headline = "COVID-19 Tracker"
   return render_template("index.html", headline = headline)
 
+@app.route ("/")
+def body():
+  import graph.py
+  return render_template("index.html", graph = graph)
 
 
-@app.route ("/resources")
+
+@app.route ("/")
 def resources():
   headline = "COVID-19 Resources"
   return render_template("index.html", headline = headline)
